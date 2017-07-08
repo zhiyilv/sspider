42c975ed8c377201cae1426b5191a2ab59275182
zhiyilv <zhiyilv@outlook.com>
2017-07-08 15:36:29 +0800

    added prep ease to prepare an enviroment

--- prep.py
+++ prep.py
@@ -1,5 +1,6 @@
 import requests as r
 from bs4 import BeautifulSoup as bs
+import re
 
 start_url = 'http://ssh.speiyou.com/search/index/grade:1/subject:/level:bx/' \
             'term:/period:/teaid:/m:/d:/time:/bg:n/nu:/service:/curpage:1'
